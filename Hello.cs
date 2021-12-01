e42aef393d5631db928ff309c94ef60e1e49d65a
Hugo Licea <hleon@elranchoinc.com>
2021-11-30 18:08:51 -0600

    Main and hello classes

---
+++ Hello.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Exercices
+{
+    class Hello
+    {
+        public Hello() { }
+        public string name(String first, String last)
+        {
+            return("Your full name is: " + first + " " + last);
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first, last;
+            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("My Firs console program");
+            Console.WriteLine("Enter your name: ");
+            first = Console.ReadLine();
+            Console.WriteLine("Enter  your last name");
+            last = Console.ReadLine();
+
+            Hello hello = new Hello();
+
+            Console.WriteLine(hello.name(first, last));
+
+
+        }
+    }
+}
