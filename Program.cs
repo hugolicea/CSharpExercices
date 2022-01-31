c34f5fe2b35acbeb1d31fbdb58c324bd6a2fa7f6
Hugo <37150395+hugolicea@users.noreply.github.com>
2022-01-31 17:30:22 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,28 @@ namespace BethanysPieShopHRM
     {
         static void Main(string[] args)
         {
+            /* string first, last;
+              Console.WriteLine("Hello World!!!");
+              Console.WriteLine("My Firs console program");
+              Console.WriteLine("Enter your name: ");
+              first = Console.ReadLine();
+              Console.WriteLine("Enter  your last name");
+              last = Console.ReadLine();
+              Hello hello = new Hello();
+              Console.WriteLine(hello.name(first, last));
+              Console.WriteLine("End Hello ---------------");
+            */
+            ValuesTypes valuesTypes = new ValuesTypes();
+            valuesTypes.Mutable();
+            Console.WriteLine("End valuesTypes ---------------");
+
+            IntegralNumericTypes integralNumericTypes = new IntegralNumericTypes();
+
+            EnumConversionExample enumConversionExample = new EnumConversionExample();
+            enumConversionExample.sesonConverter();
+
+            StructWithoutNew structWithoutNew = new StructWithoutNew();
+            structWithoutNew.initializer();
             int montlyWage = 0;
             int monts = 12;
             var bonus = 1000;
